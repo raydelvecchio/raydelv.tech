221029f1f67fc7d63228228a0da6b8143316056f
Raymond Del Vecchio <raydelv@mack-truck.mynetworksettings.com>
2023-09-06 22:17:10 -0400

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <div className="message">
+                Hey, I'm Ray.
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
