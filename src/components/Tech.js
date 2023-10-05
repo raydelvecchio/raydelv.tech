d72dda78b9c24672536cdb8d7b121812b54cc6a3
Raymond Del Vecchio <raydelv@Mack-Truck.verizon.net>
2023-10-04 21:19:57 -0400

    added mockingbird

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -20,7 +20,7 @@ function Tech() {
                                     My favorite personal project was <a className="link" href="https://github.com/raydelvecchio/SwiftAI-v2" target="_blank" rel="noopener noreferrer">SwiftAIv2</a> (Python, Huggingface, PyTorch), an LLM fine tuned to write new Taylor Swift songs (improved upon my original <a className="link" href="https://github.com/raydelvecchio/SwiftAI" target="_blank" rel="noopener noreferrer">v1</a>)
                                 </li>
                                 <li>
-                                    I'm currently working on RaveRec, an ML tool to automatically DJ a party based on crowd feedback (publishing soon!)
+                                    I'm currently working on <a className="link" href="https://mockingbird.live" target="_blank" rel="noopener noreferrer">Mockingbird</a>, an ML tool to automatically DJ a party, generate song recommendations based on crowd feedback, and manage music queuing for establishments
                                 </li>
                                 <li>
                                     Through my time coding, I've completed a number of small projects as well, such as a remote controlled water gun (Arduino, Blynk, C++), a camera control virus (Kali Linux), variational autoencoder (VAE) for similar molecule generation (Python, Tensorflow), full GUI and playable pacman (Java, JavaFX), on-chain maximum extractable value (MEV) bots (Ethers.js, web3.js, Typescript, Solidity), and more
