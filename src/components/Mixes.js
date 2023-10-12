fb73f123c94b1502d254ef7319a2ef818cef3a3c
Raymond Del Vecchio <raydelv@MackTruck.local>
2023-10-11 18:10:35 -0700

    saving

--- src/components/Mixes.js
+++ src/components/Mixes.js
@@ -7,13 +7,13 @@ function Mixes() {
             <div className="container">
                 <div className="content-wrapper">
                     <div className="message">
-                        My funny mixes.
+                        Mixes.
                     </div>
                     <div className="centered-list">
                         <div className="bullet-list-container">
                             <ul>
                                 <li>
-                                    <a className="link" href="/Electric%20Love%20Story.mp3" target="_blank" rel="noopener noreferrer">Electric Love Story</a>
+                                    <a className="link" href="/mixes/Electric%20Love%20Story.mp3" target="_blank" rel="noopener noreferrer">Electric Love Story</a>: Love Story (Taylor Swift) x Electric Love (BORNS, TWINSICK 2022 Remix)
                                 </li>
                             </ul>
                         </div>
