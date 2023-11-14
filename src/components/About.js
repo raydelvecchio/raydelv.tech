5643526a1c007617a4cef8492cb424fe16f864fe
Raymond Del Vecchio <raydelv@MackTruck.local>
2023-11-13 21:49:59 -0700

    saving work

--- src/components/About.js
+++ src/components/About.js
@@ -13,9 +13,10 @@ function About() {
                     <p>
                         Hello! Nice to meet you. My name is Ray Del Vecchio - I'm 21 years old from <a className="link" href="https://en.wikipedia.org/wiki/Clifton,_Virginia" target="_blank" rel="noopener noreferrer">Clifton, VA</a>
                          , but have lived in Los Angeles, Boston, Newport, Palo Alto, and San Francisco. I attend <a className="link" href="https://brown.edu" target="_blank" rel="noopener noreferrer">Brown University</a> studying computer science, yet
-                         I'm on a gap at the moment (to build). In elementary school, I first began playing around with terminals, writing scripts, and eventually building my first computer before high school.
+                         I'm on a gap indefinitely at the moment (to build). In elementary school, I first began playing around with terminals, writing scripts, and eventually building my first computer before high school.
                          The technological knack stuck, and now I code for a living. I greatly enjoy the problem solving, logical, precise aspects of computation and consider myself lucky
-                         I can pursue my interests for work. Though mostly in CS, my interests are quite broad, and I've taken classes in neuroscience, economics, engineering, pure/applied math, and chemistry. You can find more fun facts about me <a className="link" href="/fun" target="_blank" rel="noopener noreferrer">here</a>. Welcome to my website :)
+                         I can pursue my interests for work. Though mostly in CS, my interests are quite broad, and I've taken classes in neuroscience, economics, engineering, pure/applied math, and chemistry. At the moment,
+                        I'm optimizing my life to maximize <i>experience</i>. You can find more fun facts about me <a className="link" href="/fun" target="_blank" rel="noopener noreferrer">here</a>. Welcome to my website :)
                     </p>
                 </div>
             </div>
--- src/components/Misc.js
+++ src/components/Misc.js
@@ -17,16 +17,16 @@ function Misc() {
                                     I hold a second degree black belt in ITF Taekwondo and was technically once 3rd in the world for <a className="link" href="https://centurymartialarts.com/collections/bo-staff" target="_blank" rel="noopener noreferrer">Bo Staff</a> performance
                                 </li>
                                 <li>
-                                    I was ranked nationally in the NCFL PF debate circuit
+                                    When I can, I enjoy surfing, bouldering, and hiking, although I'm not very good at any of these activities
                                 </li>
                                 <li>
-                                    My current hobbies include surfing (when I can), bouldering (V2/3 right now), and basic music production
+                                    I was ranked nationally in the NCFL PF debate circuit
                                 </li>
                                 <li>
                                     I'm a passionate event host who enjoys meeting new people; have held social/networking events the bay, and led independent seminar-like discussions on Brown's campus on tech/ethical/social topics
                                 </li>
                                 <li>
-                                    I used to be pre-med student and took two college semesters of organic chemistry in high school
+                                    I used to be pre-med (chemistry major at Brown) and took two college semesters of organic chemistry in high school
                                 </li>
                                 <li>
                                     For the summer of 2022, I lived at <a className="link" href="https://www.businessinsider.com/facebook-house-inside-palo-alto-home-mark-zuckerberg-la-jennifer-2017-12" target="_blank" rel="noopener noreferrer">819 La Jennifer Way</a> with a bunch of GSB students
@@ -35,13 +35,13 @@ function Misc() {
                                     In 2020, I co-founded a startup <a className="link" href="https://hyphora.org" target="_blank" rel="noopener noreferrer">Hyphora</a> (now inactive) where I ran marketing and product
                                 </li>
                                 <li>
-                                    Loquor quidam latine; have taken 5 years of the language
+                                    Loquor quidam latine, and I have taken somewhere around 5 years of the language
                                 </li>
                                 <li>
-                                    I'm an amateur DJ, and DJed my first party at Harvard in September. I just landed my first paying gig!
+                                    I'm an <a className="link" href="/mix">amateur DJ</a>, and DJed my first party at Harvard in September '23. I mostly mix house music, but am slowly expanding to other genres.
                                 </li>
                                 <li>
-                                    I love <a className="link" href="https://en.wikipedia.org/wiki/Electronic_dance_music" target="_blank" rel="noopener noreferrer">EDM</a>. I've attended EZOO (2x), Countdown, Beyond Wonderland, Breakaway Carolina, Breakaway Boston, Daytrip Seattle, and Hard Summer, as well as independent shows by Diesel, Valentino Khan, Deorro, Jiqui, and Muerte. Upcoming: Escape, Apocalypse, Countdown, Thunderdome, and EDC Las Vegas.
+                                    I love <a className="link" href="https://en.wikipedia.org/wiki/Electronic_dance_music" target="_blank" rel="noopener noreferrer">EDM</a>. I've attended EZOO (2x), Countdown, Escape, Beyond Wonderland, Breakaway Carolina, Breakaway Boston, Daytrip Seattle, and Hard Summer, as well as independent shows by Diesel, Valentino Khan, Deorro, Jiqui, Galantis, and Muerte. Upcoming: Apocalypse, Thunderdome, and EDC Las Vegas.
                                 </li>
                             </ul>
                         </div>
