6ceb141cf50d666e73d10e39944fef37b0ee5ea9
Raymond Del Vecchio <xraydelv@gmail.com>
2024-10-21 15:22:44 -0700

    reverse order of writing

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -8,6 +8,11 @@ function Writing() {
                     <h1 className="text-center text-4xl font-bold mb-8">
                         I write infrequently.
                     </h1>
+                    <h2 className="text-2xl font-semibold mb-4">Writing</h2>
+                    <ul className="list-disc space-y-4 pl-6">
+                        <li><a className="underline text-link" href="https://medium.com/@raymond_del_vecchio/how-do-transformers-work-really-96fff8fe25c9" target="_blank" rel="noopener noreferrer">How Do Transformers Work, Really? A intuitive explanation.</a></li>
+                        <li><a className="underline text-link" href="/CVHS_Guide_To_Science_Fair.pdf" target="_blank" rel="noopener noreferrer">High School Guide to Science Fair</a></li>
+                    </ul>
                     <h2 className="text-2xl font-semibold mb-4">Threads</h2>
                     <ul className="list-disc space-y-4 pl-6 mb-8">
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1755364584930861562" target="_blank" rel="noopener noreferrer">VLite2 is the Fastest Vector Database</a></li>
@@ -17,11 +22,6 @@ function Writing() {
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1679968366377725953" target="_blank" rel="noopener noreferrer">How Language Models Use Long Context Windows</a></li>
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1666328599119663106" target="_blank" rel="noopener noreferrer">SEC Sues Coinbase Key Points</a></li>
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1629547395359952897" target="_blank" rel="noopener noreferrer">AI Philosophy w/ Joanne Lim</a></li>
-                    </ul>
-                    <h2 className="text-2xl font-semibold mb-4">Writing</h2>
-                    <ul className="list-disc space-y-4 pl-6">
-                        <li><a className="underline text-link" href="https://medium.com/@raymond_del_vecchio/how-do-transformers-work-really-96fff8fe25c9" target="_blank" rel="noopener noreferrer">How Do Transformers Work, Really? A intuitive explanation.</a></li>
-                        <li><a className="underline text-link" href="/CVHS_Guide_To_Science_Fair.pdf" target="_blank" rel="noopener noreferrer">High School Guide to Science Fair</a></li>
                     </ul>
                 </div>
             </div>
