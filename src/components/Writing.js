622b6ebae4dcb9d73321dcdf8efc148a3388591e
Raymond Del Vecchio <xraydelv@gmail.com>
2024-10-21 20:53:49 -0700

    spacing

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -13,7 +13,8 @@ function Writing() {
                         <li><a className="underline text-link" href="https://medium.com/@raymond_del_vecchio/how-do-transformers-work-really-96fff8fe25c9" target="_blank" rel="noopener noreferrer">How Do Transformers Work, Really? A intuitive explanation.</a></li>
                         <li><a className="underline text-link" href="/CVHS_Guide_To_Science_Fair.pdf" target="_blank" rel="noopener noreferrer">High School Guide to Science Fair</a></li>
                     </ul>
-                    <h2 className="text-2xl font-semibold mb-4">Threads</h2>
+
+                    <h2 className="text-2xl font-semibold mb-4 mt-8">Threads</h2>
                     <ul className="list-disc space-y-4 pl-6 mb-8">
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1755364584930861562" target="_blank" rel="noopener noreferrer">VLite2 is the Fastest Vector Database</a></li>
                         <li><a className="underline text-link" href="https://twitter.com/raydelvecc/status/1725317238495093094" target="_blank" rel="noopener noreferrer">Thread on NormalComputing's Baked-in RAG Alternative</a></li>
